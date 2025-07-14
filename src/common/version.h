80dc61320b689945582080844c7c3d9180fb01ff
zbynekdrlik <drlik.zbynek@gmail.com>
2025-07-14 23:57:28 +0200

    Update version to 1.1.0 for DeckLink support

---
+++ src/common/version.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#define NDI_BRIDGE_VERSION_MAJOR 1
+#define NDI_BRIDGE_VERSION_MINOR 1
+#define NDI_BRIDGE_VERSION_PATCH 0
+
+#define NDI_BRIDGE_VERSION_STRING "1.1.0"
+#define NDI_BRIDGE_VERSION NDI_BRIDGE_VERSION_STRING
+
+// Build info
+#ifdef _DEBUG
+#define NDI_BRIDGE_BUILD_TYPE "Debug"
+#else
+#define NDI_BRIDGE_BUILD_TYPE "Release"
+#endif
+
+#ifdef _WIN64
+#define NDI_BRIDGE_PLATFORM "Windows x64"
+#elif defined(_WIN32)
+#define NDI_BRIDGE_PLATFORM "Windows x86"
+#elif defined(__linux__)
+#define NDI_BRIDGE_PLATFORM "Linux"
+#else
+#define NDI_BRIDGE_PLATFORM "Unknown"
+#endif
+
+// Feature flags
+#define NDI_BRIDGE_HAS_MEDIA_FOUNDATION 1
+#define NDI_BRIDGE_HAS_DECKLINK 1
+#define NDI_BRIDGE_HAS_V4L2 0
