29106aff19d74f6f675aa5c33828e2bc08aa4f17
zbynekdrlik <drlik.zbynek@gmail.com>
2025-07-14 19:18:32 +0200

    Update version to 1.0.4

--- src/common/version.h
+++ src/common/version.h
@@ -1,11 +1,24 @@
-#ifndef NDI_BRIDGE_VERSION_H
-#define NDI_BRIDGE_VERSION_H
+#pragma once
 
 #define NDI_BRIDGE_VERSION_MAJOR 1
 #define NDI_BRIDGE_VERSION_MINOR 0
-#define NDI_BRIDGE_VERSION_PATCH 3
+#define NDI_BRIDGE_VERSION_PATCH 4
 
-#define NDI_BRIDGE_VERSION_STRING "1.0.3"
-#define NDI_BRIDGE_VERSION "1.0.3"
+#define NDI_BRIDGE_VERSION_STRING "1.0.4"
 
-#endif // NDI_BRIDGE_VERSION_H
+// Build info
+#ifdef _DEBUG
+#define NDI_BRIDGE_BUILD_TYPE "Debug"
+#else
+#define NDI_BRIDGE_BUILD_TYPE "Release"
+#endif
+
+#ifdef _WIN64
+#define NDI_BRIDGE_PLATFORM "Windows x64"
+#elif defined(_WIN32)
+#define NDI_BRIDGE_PLATFORM "Windows x86"
+#elif defined(__linux__)
+#define NDI_BRIDGE_PLATFORM "Linux"
+#else
+#define NDI_BRIDGE_PLATFORM "Unknown"
+#endif
