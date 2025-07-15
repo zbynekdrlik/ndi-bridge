5c3a59718d1fd5db79948857ce6e69620e72868b
zbynekdrlik <drlik.zbynek@gmail.com>
2025-07-15 09:19:09 +0200

    Update version to 1.1.1 - Fixed DeckLink integration

---
+++ include/common/version.h
@@ -0,0 +1,21 @@
+// version.h
+#pragma once
+
+#define NDI_BRIDGE_VERSION "1.1.1"
+#define NDI_BRIDGE_VERSION_MAJOR 1
+#define NDI_BRIDGE_VERSION_MINOR 1
+#define NDI_BRIDGE_VERSION_PATCH 1
+
+#ifdef _WIN32
+#define NDI_BRIDGE_PLATFORM "Windows"
+#elif __linux__
+#define NDI_BRIDGE_PLATFORM "Linux"
+#else
+#define NDI_BRIDGE_PLATFORM "Unknown"
+#endif
+
+#ifdef _DEBUG
+#define NDI_BRIDGE_BUILD_TYPE "Debug"
+#else
+#define NDI_BRIDGE_BUILD_TYPE "Release"
+#endif
