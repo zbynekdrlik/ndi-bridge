bd4c7ab08deffab3809f5086382cb9f2d2dd43af
zbynekdrlik <drlik.zbynek@gmail.com>
2025-07-15 14:26:24 +0200

    Update version to 1.1.7 - NZXT workaround

--- include/common/version.h
+++ include/common/version.h
@@ -1,10 +1,10 @@
 // version.h
 #pragma once
 
-#define NDI_BRIDGE_VERSION "1.1.6"
+#define NDI_BRIDGE_VERSION "1.1.7"
 #define NDI_BRIDGE_VERSION_MAJOR 1
 #define NDI_BRIDGE_VERSION_MINOR 1
-#define NDI_BRIDGE_VERSION_PATCH 6
+#define NDI_BRIDGE_VERSION_PATCH 7
 
 #ifdef _WIN32
 #define NDI_BRIDGE_PLATFORM "Windows"
