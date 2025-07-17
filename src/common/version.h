f63288e84948172fc9915394c7a8b86858ad0803
zbynekdrlik <drlik.zbynek@gmail.com>
2025-07-17 14:31:51 +0200

    Fix version constants for main.cpp compilation errors

--- src/common/version.h
+++ src/common/version.h
@@ -5,3 +5,21 @@
 #define VERSION_PATCH 6
 
 #define VERSION_STRING "1.6.6"
+
+// Additional defines for main.cpp compatibility
+#define NDI_BRIDGE_VERSION VERSION_STRING
+
+// Build type and platform detection
+#ifdef _DEBUG
+#define NDI_BRIDGE_BUILD_TYPE "Debug"
+#else
+#define NDI_BRIDGE_BUILD_TYPE "Release"
+#endif
+
+#ifdef _WIN32
+#define NDI_BRIDGE_PLATFORM "Windows"
+#elif defined(__linux__)
+#define NDI_BRIDGE_PLATFORM "Linux"
+#else
+#define NDI_BRIDGE_PLATFORM "Unknown"
+#endif
