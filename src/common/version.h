d26c9115a3c02e9ee072a3484fc2462f0e542c80
zbynekdrlik <drlik.zbynek@gmail.com>
2025-07-17 16:59:56 +0200

    Bump version to 1.7.0 for Linux V4L2 latency improvements

--- src/common/version.h
+++ src/common/version.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 6
-#define VERSION_PATCH 7
+#define VERSION_MINOR 7
+#define VERSION_PATCH 0
 
-#define VERSION_STRING "1.6.7"
+#define VERSION_STRING "1.7.0"
 
 // Additional defines for main.cpp compatibility
 #define NDI_BRIDGE_VERSION VERSION_STRING
