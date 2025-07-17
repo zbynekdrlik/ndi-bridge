9cdfb91fe227b919bba4def2f712588568331fc5
zbynekdrlik <drlik.zbynek@gmail.com>
2025-07-17 22:50:50 +0200

    Bump version to 1.7.1 - fixed critical latency issues

--- src/common/version.h
+++ src/common/version.h
@@ -2,9 +2,9 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 7
-#define VERSION_PATCH 0
+#define VERSION_PATCH 1
 
-#define VERSION_STRING "1.7.0"
+#define VERSION_STRING "1.7.1"
 
 // Additional defines for main.cpp compatibility
 #define NDI_BRIDGE_VERSION VERSION_STRING
