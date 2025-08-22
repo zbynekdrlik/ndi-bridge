6fdef25a00113bde74d9ace5b723c377d68f80e8
zbynekdrlik <drlik.zbynek@gmail.com>
2025-08-22 21:54:35 +0200

    chore: Bump build version to 1.8.6 for audio package fix verification

--- scripts/build-modules/00-variables.sh
+++ scripts/build-modules/00-variables.sh
@@ -3,7 +3,7 @@
 # This module defines all global variables used throughout the build process
 
 # Build Script Version - Auto-incremented with each build
-BUILD_SCRIPT_VERSION="1.8.5"
+BUILD_SCRIPT_VERSION="1.8.6"
 BUILD_SCRIPT_DATE="2025-08-22"
 
 # Build timestamp - Generated at build time (local timezone)
