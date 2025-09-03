085ac8978d1cab45ff599ffb6c5bb24019cba788
zbynekdrlik <drlik.zbynek@gmail.com>
2025-09-03 12:36:37 +0200

    Increment version to 2.2.2 after main merge with PipeWire services

--- scripts/build-modules/00-variables.sh
+++ scripts/build-modules/00-variables.sh
@@ -3,7 +3,7 @@
 # This module defines all global variables used throughout the build process
 
 # Build Script Version - Auto-incremented with each build
-BUILD_SCRIPT_VERSION="2.2.0"
+BUILD_SCRIPT_VERSION="2.2.2"
 BUILD_SCRIPT_DATE="2025-09-03"
 
 # Build timestamp - Generated at build time (local timezone)
